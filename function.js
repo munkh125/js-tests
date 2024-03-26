6006ea1a0056d7a5f070d5ed1d0c09067acb0535
munkh125 <munkh125@gmail.com>
2024-03-26 12:26:50 +0800

    added array.js

---
+++ function.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    alert('Hello');
+}
+
+myFunction();
+
+//anonymous function
+(function () {
+    alert('hello anonymous');
+})
---
+++ test.js
@@ -0,0 +1,6 @@
+const removeFromArray = function (array, ...args) {
+  console.log(array);
+  console.log(...args);
+
+};
+removeFromArray([1, 2, 3, 4], 3);
