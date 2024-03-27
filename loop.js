efb19c8c4ec37c7caba756a4b9695abe2afeb0be
munkh125 <munkh125@gmail.com>
2024-03-27 11:52:06 +0800

    added loop

---
+++ loop.js
@@ -0,0 +1,24 @@
+for(let i = 0; i < 10; i++){
+    console.log(`For : ${i}`);
+}
+
+for(let i = 0; i < 10; i++){
+    if(i == 5){
+        break; //exits when i = 5 
+        //continue; skips 5
+    }
+    console.log(`break : ${i}`);
+}
+
+let i = 0;
+while(i < 10){
+    console.log(`While : ${i}`);
+    i++;
+}
+
+//executes atleast one
+let j = 0; 
+do{
+    console.log(`do while: ${j}`);
+    j++
+}while(j<10);
