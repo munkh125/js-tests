4e6beafea8baa9a2e401d736d43d465a0f0c664b
munkh125 <munkh125@gmail.com>
2024-04-10 11:56:44 +0800

    corrected wrong words

--- array.js
+++ array.js
@@ -1,11 +1,7 @@
 //Creating a array
 const cars = ["Saab", "Volvo", "BMW"];
 
-const japan_cars = [
-    "Nissan",
-    "Honda",
-    "Subaru"
-];
+const japan_cars = ["Nissan", "Honda", "Subaru"];
 
 const american_cars = [];
 american_cars[0] = "Ford";
@@ -13,7 +9,7 @@ american_cars[1] = "Chevrolet";
 
 const electric_cars = new Array("Tesla", "BMW", "Chevrolet");
 
-let electric_car = electric_cars[0];//Tesla
+let electric_car = electric_cars[0]; //Tesla
 
 //Changing array element
 japan_cars[0] = "Toyota";
@@ -22,18 +18,18 @@ japan_cars[0] = "Toyota";
 let arrayToString = cars.toString();
 
 //Arrays are Objects
-const person = {firstName:"John", lastName:"Doe", age:46};
+const person = { firstName: "John", lastName: "Doe", age: 46 };
 person.firstName;
 
 //Looping Array Elements
-for(let i=0;i<cars.length;i++){
-    console.log(cars[i]);
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
 }
 
-let carFor="";
+let carFor = "";
 japan_cars.forEach(myFunction);
-function myFunction(value){
-    carFor += `${value}`
+function myFunction(value) {
+  carFor += `${value}`;
 }
 console.log(carFor);
 
@@ -51,20 +47,20 @@ console.log(cars instanceof Array);
 
 //Array length
 let size = cars.length; //5
-console.log(`Cars array: ${cars}`)
+console.log(`Cars array: ${cars}`);
 console.log(`Cars array length: ${size}`);
 
 //toString()
-console.log(cars.toString());//Saab,Volvo,BMW,MAN,Subaru
+console.log(cars.toString()); //Saab,Volvo,BMW,MAN,Subaru
 
 //at()
-let secondCar = cars.at(2);//BMW third element
+let secondCar = cars.at(2); //BMW third element
 console.log(secondCar);
 let secondCarA = cars[2];
 console.log(secondCarA);
 
 //join()
-let join = cars.join(" * ");//Saab * Volvo * BMW * MAN * Subaru
+let join = cars.join(" * "); //Saab * Volvo * BMW * MAN * Subaru
 console.log(join);
 
 //pop() removes last element from array returns removed element
@@ -107,37 +103,41 @@ const alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 let copyWithin = alphabets.copyWithin(2, 0); //index 0 ees 2luu huul
 //['A', 'B', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 console.log(copyWithin);
-let copyWithin2 = alphabets.copyWithin(2, 0, 3);// A B A gh met 
+let copyWithin2 = alphabets.copyWithin(2, 0, 3); // A B A gh met
 //['A', 'B', 'A', 'B', 'A', 'D', 'E', 'F', 'G', 'H']
 console.log(copyWithin2);
 
 //flat() creates a new array with sub-array elements concatenated to a specified depth.
-const myArr = [[1,2],[3,4],[5,6]];
+const myArr = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
 const flat = myArr.flat();
 //[1, 2, 3, 4, 5, 6]
 console.log(flat);
 
 //splice() add new items to an array returns an array with the deleted items
-const numbers = ["1","2","3","4","5"];
+const numbers = ["1", "2", "3", "4", "5"];
 let splice = numbers.splice(2, 2, "6", "7");
 //first parameter (2) defines the position where new elements should be added
-//second parameter (0) defines how many elements should be removed 
+//second parameter (0) defines how many elements should be removed
 //rest of parameters define the new elements to be added
 console.log(numbers);
 console.log(splice);
-let spliceRemove = numbers.splice(0,1);//remove element usin splice
+let spliceRemove = numbers.splice(0, 1); //remove element usin splice
 console.log(numbers);
 console.log(spliceRemove);
 //toSpliced() creates a new array keeping original array unchanged
-const months = ["Jan","Feb","Mar","Apr"];
-const spliced = months.toSpliced(0,1);
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const spliced = months.toSpliced(0, 1);
 console.log(months);
 console.log(spliced);
 
 //slice() slices out a piece of an array into a new array
-const fruits = ["Banana","Orange","Lemon","Apple","Mango"];
-const slice = fruits.slice(1);//['Orange', 'Lemon', 'Apple', 'Mango']
-const slice2 = fruits.slice(1, 3)//['Orange', 'Lemon']
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const slice = fruits.slice(1); //['Orange', 'Lemon', 'Apple', 'Mango']
+const slice2 = fruits.slice(1, 3); //['Orange', 'Lemon']
 console.log(fruits);
 console.log(slice);
 console.log(slice2);
@@ -151,29 +151,29 @@ console.log(`Index of apple is: ${position}`);
 //lastIndexOf() same bur searches from behind
 
 //includes()
-console.log(fruits.includes("Banana"));//true
+console.log(fruits.includes("Banana")); //true
 
 //find()
-const number = [4,9,16,25,36];
+const number = [4, 9, 16, 25, 36];
 let first = number.find(findFunction);
-function findFunction(value, index, array){
-    return value > 18;
+function findFunction(value, index, array) {
+  return value > 18;
 }
-console.log(first);//25
+console.log(first); //25
 //findIndex() returns the index of the first array element that passes a test function
 //3
 
-// findLast() start from the end of an array and 
+// findLast() start from the end of an array and
 // return the value of the first element that satisfies a condition
 const temp = [27, 28, 30, 40, 42, 35, 30];
-let high = temp.findLast(x=> x > 40);
+let high = temp.findLast((x) => x > 40);
 console.log(`first element higher than 40 is ${high}`);
 //findLastIndex() finds the index of the last element that satisfies a condition
-let pos = temp.findLastIndex(x => x > 40);
+let pos = temp.findLastIndex((x) => x > 40);
 console.log(`index of first element higher than 40 is ${pos}`);
 
 // ---------------- SORT ---------------------
-const sortArray = ["Banana","Apple","Orange","Mango"];
+const sortArray = ["Banana", "Apple", "Orange", "Mango"];
 //sort() sorts an array alphabetically
 //toSorted(); safe way to sort
 console.log(sortArray);
@@ -182,26 +182,30 @@ const sorted = sortArray.toSorted();
 console.log(sorted);
 //reverse() reverse the elemets in an array
 //toReversed() safe way to reverse
-const points = [40,100,1,5,25,10];
+const points = [40, 100, 1, 5, 25, 10];
 console.log(points.sort());
-console.log(points.sort(function(a,b){return a - b}));
+console.log(
+  points.sort(function (a, b) {
+    return a - b;
+  })
+);
 //[1, 5, 10, 25, 40, 100]
 
 // ---------------- ITERATION ---------------------
 //forEach() calls a function once for each array element
-const data = [1,4,9,16,25,36];
+const data = [1, 4, 9, 16, 25, 36];
 let txt = "";
 data.forEach(forFunction);
-function forFunction(value,index,array){
-    txt += value + "\n";
+function forFunction(value, index, array) {
+  txt += value + "\n";
 }
 console.log(data);
 console.log(txt);
 
 //map() creates new array
 const data2 = data.map(mapFunction);
-function mapFunction(value,index,array){
-    return value * 2;
+function mapFunction(value, index, array) {
+  return value * 2;
 }
 console.log(data2);
 
@@ -212,50 +216,49 @@ console.log(data3);
 //filter() creates a new array with array elemets that pass a test
 const over18 = data.filter(filterFunction);
 
-function filterFunction(value, index, array){
-    return value > 18;
+function filterFunction(value, index, array) {
+  return value > 18;
 }
-console.log(over18);//[25, 36]
+console.log(over18); //[25, 36]
 
 //reduce() redice array to single value, doesn reduce original array
 //reduceRight()
 let reduce = data.reduce(reduceFunction);
-function reduceFunction(total, value, index, array){
-    return total + value;
+function reduceFunction(total, value, index, array) {
+  return total + value;
 }
 console.log(reduce);
-let reduce2 = data.reduce(reduceFunction, 100);//can add initial value
+let reduce2 = data.reduce(reduceFunction, 100); //can add initial value
 console.log(reduce2);
 
 //every() checks if all array values pass a test
 let allOver18 = data.every(everyFunction);
-function everyFunction(value, inex, array){
-    return value > 18;
+function everyFunction(value, index, array) {
+  return value > 18;
 }
-console.log(allOver18);//false
+console.log(allOver18); //false
 
 //some checkhs if some array values pass a test
 let someOver18 = data.some(everyFunction);
-console.log(someOver18);//true
+console.log(someOver18); //true
 
 //Array.from() create an array from a string
 console.log(Array.from("ABCDEFG"));
 
 //Array.keys() returns an array iterator object with the keys of an array
-//entries() create an array iterator and then iterate over the key/value pairs 
+//entries() create an array iterator and then iterate over the key/value pairs
 //[0, "Banana"]
 //[1, "Orange"]
 
 //with() update elements in an array without altering the original array
-const data4 = data.with(2, "10")
+const data4 = data.with(2, "10");
 console.log(data4);
 
 //... spread expands an iterable into more elements
-const q1 = ['jan','feb','mar'];
-const q2 = ['apr','may','jun'];
-const q3 = ['jul','aug','sep'];
-const q4 = ['oct','nov','dec'];
+const q1 = ["jan", "feb", "mar"];
+const q2 = ["apr", "may", "jun"];
+const q3 = ["jul", "aug", "sep"];
+const q4 = ["oct", "nov", "dec"];
 
 const year = [...q1, ...q2, ...q3, ...q4];
-console.log(year);//['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-
+console.log(year); //['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
--- arraytest.js
+++ arraytest.js
@@ -1,7 +1,6 @@
-const arrayTest = function (ar,array1, ...args) {
+const arrayTest = function (ar, array1, ...args) {
   console.log(ar);
-  console.log(array1)
+  console.log(array1);
   console.log(...args);
-
 };
 arrayTest([1, 2, 3, 4], [3, 5], 6);
